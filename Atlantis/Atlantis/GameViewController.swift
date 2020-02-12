906e17d1a74142774091fda63dc436dc4f6a6e50
Daniel Vargas <dvargasinbox@gmail.com>
2020-02-12 04:06:33 -0600

    My initial project setup

---
+++ Atlantis/Atlantis/GameViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GameViewController.swift
+//  Atlantis
+//
+//  Created by Daniel Vargas on 2/12/20.
+//  Copyright © 2020 Paradias. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
---
+++ Atlantis/AtlantisTests/AtlantisTests.swift
@@ -0,0 +1,34 @@
+//
+//  AtlantisTests.swift
+//  AtlantisTests
+//
+//  Created by Daniel Vargas on 2/12/20.
+//  Copyright © 2020 Paradias. All rights reserved.
+//
+
+import XCTest
+@testable import Atlantis
+
+class AtlantisTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
